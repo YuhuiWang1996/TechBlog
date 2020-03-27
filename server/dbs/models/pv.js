43f3b15a2f7cb29c2b255659a09f6ed546dc3149
Yuhui <yuhui@nyu.edu>
2020-03-26 23:54:04 -0500

    流量监控新增获取ip

--- server/dbs/models/pv.js
+++ server/dbs/models/pv.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const PvSchema = new Schema({
+  ip: {
+    type: String,
+    require: false
+  },
   url: {
     type: String,
     require: true
--- server/middleware/pv.js
+++ server/middleware/pv.js
@@ -6,7 +6,9 @@ module.exports = function () {
     const reg = /\/api/
     if (ctx.request.url.match(reg)) {
       try {
+        let ip = getClientIP(ctx.req);
         await Pv.create({
+          ip: ip,
           url: ctx.request.url,
           createAt: moment().format('YYYY-MM-DD HH:mm:ss'),
         })
@@ -17,3 +19,10 @@ module.exports = function () {
     await next()
   }
 }
+
+function getClientIP(req) {
+  return req.headers['x-forwarded-for'] || // 判断是否有反向代理 IP
+    req.connection.remoteAddress || // 判断 connection 的远程 IP
+    req.socket.remoteAddress || // 判断后端的 socket 的 IP
+    req.connection.socket.remoteAddress || '0.0.0.0';
+};
